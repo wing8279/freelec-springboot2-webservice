6392a393fd1f3f3dd6e1795568b7c3c576b572c2
wing8279 <wing8279@futurewiz.co.kr>
2021-02-03 19:47:40 +0900

    dto 추가 및 테스트 코드 에러로 gradlew wrapper --gradle-version 4.10.2 으로 다운그레이드

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -12,19 +12,41 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+//테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킵니다.
+//여기서는 SpringRunner라는 스프링 실행자를 사용합니다.
+//즉, 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 합니다.
 @RunWith(SpringRunner.class)
+
+//여러 스프링 테스트 어노테이션 중, Web에 집중할 수 있는 어노테이션 입니다.
+//선언할 경우 @Controller, @ControllerAdvice 등을 사용할 수 있고,
+//@Service, @Component, @Repository 등은 사용할 수 없습니다.
+//여기서는 컨트롤러만 사용하기 때문에 선언합니다.
 @WebMvcTest
 public class HelloControllerTest {
 
+    //스프링이 관리하는 빈(Bean)을 주입 받습니다.
     @Autowired
+    //웹 API를 테스트할 때 사용합니다.
+    //스프링 MVC 테스트의 시작점입니다.
+    //이 클래스를 통해 HTTP GET, POST등에 대한 API 테스트를 할 수 있습니다.
     private MockMvc mvc;
+
 
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
+        //MockMvc를 통해 /hello 주소로 HTTP GET 요청을 합니다.
+        //체이닝이 진원되어 아래와 같이 여러 검증 기능을 이어서 선언할 수 있습니다.
         mvc.perform(get("/hello"))
+                //mvc.perform의 결과를 검증합니다.
+                //HTTP Header의 Status를 검증합니다.
+                //우리가 흔히 알고 있는 200, 404, 500등의 상태를 검증합니다.
+                //여기선 OK 즉, 200인지 아닌지를 검증합니다.
                 .andExpect(status().isOk())
+                //mvc.perform의 결과를 검증합니다.
+                //응답 본문의 내용을 검증합니다.
+                //Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는 검증합니다.
                 .andExpect(content().string(hello));
 
     }
