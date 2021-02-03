c323514a3701dd8f302f41781512d1e84540e894
wing8279 <wing8279@futurewiz.co.kr>
2021-02-03 20:33:39 +0900

    helloDto 및 테스트 코드 추가

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -8,8 +8,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킵니다.
@@ -31,7 +33,6 @@ public class HelloControllerTest {
     //이 클래스를 통해 HTTP GET, POST등에 대한 API 테스트를 할 수 있습니다.
     private MockMvc mvc;
 
-
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -50,4 +51,26 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
 
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        //param : API 테스트할 때 사용될 요청 파라미터를 설정합니다.
+        //단, 값은 String만 허용됩니다.
+        //그래서 숫자/날짜 등의 데이터도 등록할 때는 문자열로 변경해야만 가능합니다.
+
+        //jsonPath : JSON 응답값을 필드별로 검증할 수 있는 메소드입니다.
+        //$를 기준으로 필드명을 명시합니다.
+        //여기서는 name과 amount를 검증하니 $.name, $.amount로 검증합니다.
+        mvc.perform(
+                    get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
 }
